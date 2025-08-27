5472d685722a792fa87ac63af541be22534468c6
austenyt <austenyt@gmail.com>
2025-08-28 00:38:08 +0300

    Commit

--- models.py
+++ models.py
@@ -23,4 +23,18 @@ class Base(Model):
     class Meta:
         database = db
 
-class
+
+class User(Base):
+    first_name = CharField()
+    last_name = CharField()
+    email = CharField(unique=True)
+    password_hash = CharField()
+    image_path = CharField(null=True)
+
+
+class Blog(Base):
+
+
+
+db.connect()
+db.create_tables(User)
